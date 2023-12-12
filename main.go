9071a4c4653376a92049ca998bd77e7d41d306c4
zhenghao <120950827@qq.com>
2023-12-13 00:17:33 +0800

    [zh] feat: 20231213

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
+type User struct {
+	gorm.Model
+	Name string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"type:varchar(110);not null;unique"`
+	Password string `gorm:"type:varchar(255);not null"`
+}
+
 func main() {
+	db := InitDB()
+	defer db.Close()
 	r := gin.Default()
 	r.POST("/api/auth/register", func(ctx *gin.Context) {
 		//获取参数
@@ -28,13 +39,34 @@ func main() {
 			name = RandomString(10)
 		}
 		log.Println(name, telephone, password)
+		//判断手机号是否存在
+		if isTelephoneExist(db, telephone) {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
+			return
+		}
 		//创建用户
+		newUser := User{
+			Name: name,
+			Telephone: telephone,
+			Password: password,
+		}
+
+
 		//返回结果
 		ctx.JSON(200, gin.H{
 			"msg": "注册成功",
 		})
 	})
 	r.Run()
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
 }
 
 func RandomString(n int) string {
@@ -46,4 +78,26 @@ func RandomString(n int) string {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
+}
+
+func InitDB() *gorm.DB {
+	driverName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "ginessential"
+	username := "root"
+	password := "root"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(driverName, args)
+	if err != nil {
+		panic("failed to connect database err:" + err.Error())
+	}
+	return db
 }
