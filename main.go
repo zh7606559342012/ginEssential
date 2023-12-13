423fcef44102d8ccc35af67428eb2a8629e08a35
astin435@gmail.com <admin@example.com>
2023-12-13 20:18:08 +0800

    [zh]20231213_02

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"oceanlearn.teach/ginessential/common"
-	"oceanlearn.teach/ginessential/controller"
 )
 
 func main() {
 	db := common.InitDB()
 	defer db.Close()
 	r := gin.Default()
-	r.POST("/api/auth/register", controller.Register)
+	r = CollectRoute(r)
+
 	r.Run()
 }
