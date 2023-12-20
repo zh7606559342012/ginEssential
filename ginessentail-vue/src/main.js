b3495a2af151efee2e9cbc4dc4e70ab9d37f21df
astin435@gmail.com <admin@example.com>
2023-12-20 09:06:39 +0800

    [zh]:20231220

--- ginessentail-vue/src/main.js
+++ ginessentail-vue/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import './assets/scss/index.scss';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
